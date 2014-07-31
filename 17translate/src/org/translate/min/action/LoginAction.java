d281adc69a5b8cb488edd708bfc9b2994398f727
Administrator <Administrator@USER-IA0V5AD0NJ>
2014-07-31 17:29:11 +0800

    shimin

---
+++ 17translate/src/org/translate/min/action/LoginAction.java
@@ -0,0 +1,97 @@
+package org.translate.min.action;
+
+import java.util.Map;
+
+
+import org.apache.struts2.interceptor.RequestAware;
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.opensymphony.xwork2.ActionSupport;
+ 
+public class LoginAction extends ActionSupport implements RequestAware,SessionAware
+{
+
+	private static final long serialVersionUID = 1L;
+	
+	private String username;
+	private String password;
+	private String identifyCode;
+	private String jsonResult;
+
+	private Map<String, Object> session;
+
+	private Map<String, Object> request;
+	
+	
+	public String identifyLogin()
+	{
+		//获取后台产生的验证码
+		String generateCode = (String)session.get("identifyCode");
+		
+		System.out.println("username:" + username + "\npassword:" + password +
+				"\nidentifyCode:" + identifyCode );
+		
+		
+		jsonResult="";
+		return SUCCESS;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//setter and getter
+	public String getUsername()
+	{
+		return username;
+	}
+	public void setUsername(String username)
+	{
+		this.username = username;
+	}
+	public String getPassword()
+	{
+		return password;
+	}
+	public void setPassword(String password)
+	{
+		this.password = password;
+	}
+	public String getIdentifyCode()
+	{
+		return identifyCode;
+	}
+	public void setIdentifyCode(String identifyCode)
+	{
+		this.identifyCode = identifyCode;
+	}
+	public void setSession(Map<String, Object> session)
+	{
+		this.session = session;
+		
+	}
+	public void setRequest(Map<String, Object> request)
+	{
+		this.request = request;
+		
+	}
+	public String getJsonResult()
+	{
+		return jsonResult;
+	}
+	public void setJsonResult(String jsonResult)
+	{
+		this.jsonResult = jsonResult;
+	}
+	
+	
+}
---
+++ 17translate/src/org/translate/min/entity/FreeTranslator.java
@@ -0,0 +1,80 @@
+package org.translate.min.entity;
+
+public class FreeTranslator
+{
+	private String fuserName;
+	private String fpassword;
+	private String fmailBox;
+	private String fphoneNumber;
+	private String fphoto;
+	private String fselfDescription;
+	private String ffan;
+	private String ftranslationCoin;
+	
+	public String getFuserName()
+	{
+		return fuserName;
+	}
+	public void setFuserName(String fuserName)
+	{
+		this.fuserName = fuserName;
+	}
+	public String getFpassword()
+	{
+		return fpassword;
+	}
+	public void setFpassword(String fpassword)
+	{
+		this.fpassword = fpassword;
+	}
+	public String getFmailBox()
+	{
+		return fmailBox;
+	}
+	public void setFmailBox(String fmailBox)
+	{
+		this.fmailBox = fmailBox;
+	}
+	public String getFphoneNumber()
+	{
+		return fphoneNumber;
+	}
+	public void setFphoneNumber(String fphoneNumber)
+	{
+		this.fphoneNumber = fphoneNumber;
+	}
+	public String getFphoto()
+	{
+		return fphoto;
+	}
+	public void setFphoto(String fphoto)
+	{
+		this.fphoto = fphoto;
+	}
+	public String getFselfDescription()
+	{
+		return fselfDescription;
+	}
+	public void setFselfDescription(String fselfDescription)
+	{
+		this.fselfDescription = fselfDescription;
+	}
+	public String getFfan()
+	{
+		return ffan;
+	}
+	public void setFfan(String ffan)
+	{
+		this.ffan = ffan;
+	}
+	public String getFtranslationCoin()
+	{
+		return ftranslationCoin;
+	}
+	public void setFtranslationCoin(String ftranslationCoin)
+	{
+		this.ftranslationCoin = ftranslationCoin;
+	}
+	
+	
+}
