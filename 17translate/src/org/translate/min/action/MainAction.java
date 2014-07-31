d501733f9f293aeeed78bd67a8db3a9e7130d4ef
Administrator <Administrator@USER-IA0V5AD0NJ>
2014-07-31 13:44:58 +0800

    shimin

---
+++ 17translate/src/org/translate/min/action/MainAction.java
@@ -0,0 +1,15 @@
+package org.translate.min.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class MainAction extends ActionSupport
+{
+
+	private static final long serialVersionUID = 1L;
+	@Override
+	public String execute() throws Exception
+	{
+		return SUCCESS;
+	}
+	
+}
